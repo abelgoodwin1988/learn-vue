90b9ce96b00b6e92a3eb56075e4c1b2d1cb8f951
Abel Goodwin <abelgoodwin1988@gmail.com>
2017-10-15 21:29:46 -0600

    state

--- vue-app/resources/views/welcome.blade.php
+++ vue-app/resources/views/welcome.blade.php
@@ -17,7 +17,6 @@
 
       <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
       <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
-
       <script src="/js/app.js"></script>
     </body>
 </html>
